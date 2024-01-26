3de21cd9e2244aa968f0fe1acf8309febce1203b
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-01-26 14:08:17 -0800

    set up JS IDs

---
+++ scripts/app.js
@@ -0,0 +1,55 @@
+HelloInput = document.getElementById("HelloInput");
+HelloCall = document.getElementById("HelloCall");
+HelloResponse = document.getElementById("HelloResponse");
+
+AddNum1 = document.getElementById("AddNum1");
+AddNum2 = document.getElementById("AddNum2");
+AddCall = document.getElementById("AddCall");
+AddResponse = document.getElementById("AddResponse");
+
+SQName = document.getElementById("SQName");
+SQTime = document.getElementById("SQTime");
+SQCall = document.getElementById("SQCall");
+SQResponse = document.getElementById("SQResponse");
+
+GOLTNum1 = document.getElementById("GOLTNum1");
+GOLTNum2 = document.getElementById("GOLTNum2");
+GOLTCall = document.getElementById("GOLTCall");
+GOLTResponse = document.getElementById("GOLTResponse");
+
+MadLib1 = document.getElementById("MadLib1");
+MadLib2 = document.getElementById("MadLib2");
+MadLib3 = document.getElementById("MadLib3");
+MadLib4 = document.getElementById("MadLib4");
+MadLib5 = document.getElementById("MadLib5");
+MadLib6 = document.getElementById("MadLib6");
+MadLib7 = document.getElementById("MadLib7");
+MadLib8 = document.getElementById("MadLib8");
+MadLib9 = document.getElementById("MadLib9");
+MadLib10 = document.getElementById("MadLib10");
+MadLibCall = document.getElementById("MadLibCall");
+MadLibResponse = document.getElementById("MadLibResponse");
+
+OOENum = document.getElementById("OOENum");
+OOECall = document.getElementById("OOECall");
+OOEResponse = document.getElementById("OOEResponse");
+
+RTPhrase = document.getElementById("RTPhrase");
+RTCall = document.getElementById("RTCall");
+RTResponse = document.getElementById("RTResponse");
+
+RNNum = document.getElementById("RNNum");
+RNCall = document.getElementById("RNCall");
+RNResponse = document.getElementById("RNResponse");
+
+M8BQuestion = document.getElementById("M8BQuestion");
+M8BCall = document.getElementById("M8BCall");
+M8BResponse = document.getElementById("M8BResponse");
+
+RPCategory = document.getElementById("RPCategory");
+RPCall = document.getElementById("RPCall");
+RPResponse = document.getElementById("RPResponse");
+
+
+
+// bb
